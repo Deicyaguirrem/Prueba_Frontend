5b2983ff71865d3f9f269a82ffbe9eb4edf1a419
Deicy Aguirre <deicyluck.1381@gmail.com>
2025-04-24 01:47:20 -0500

    Implementación prueba de Integración

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "./service/apiService"; 
 
 const App = () => {
-    return <h1>Bienvenido a la App</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Bienvenido a la App</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import * as apiService from "./service/apiService";
+
+jest.mock("./service/apiService"); 
+
+beforeEach(() => {
+  apiService.fetchData.mockResolvedValue([
+    { id: 1, name: "John Doe", email: "johndoe@example.com" },
+    { id: 2, name: "Jane Smith", email: "janesmith@example.com" },
+  ]);
+});
+
+test("Carga y muestra datos correctamente desde la API", async () => {
+  render(<App />);
+
+  await waitFor(() => expect(screen.getByText("John Doe")).toBeInTheDocument());
+  await waitFor(() => expect(screen.getByText("Jane Smith")).toBeInTheDocument());
+});
 
 test('renders learn react link', () => {
   render(<App />);
